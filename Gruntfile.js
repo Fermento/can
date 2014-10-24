ef5eac76f64500c6ecca86ef605c30d87a7ac4df
luizjr8 <luiz@fermen.to>
2014-10-23 20:12:03 -0400

    Some changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,79 @@
-module.exports = function(grunt) {
+'use strict';
+
+module.exports = function (grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    // Atividades concorrentes
-    concurrent: {
-      dev: {
-        tasks: ['nodemon', 'watch'],
-        options: {
-          logConcurrentOutput: true
-        }
-      }
-    },
-
-    // Nodemon (Rodando o Node)
-    nodemon: {
-      dev: {
-        script: '<%= pkg.main %>',
-        options: {
-          nodeArgs: ['--debug'],
-          callback: function (nodemon) {
-            nodemon.on('log', function (event) {
-              console.log(event.colour);
-            });
-            nodemon.on('restart', function () {
-              // Delay before server listens on port
-              setTimeout(function() {
-                require('fs').writeFileSync('.rebooted', 'rebooted');
-              }, 1000);
-            });
-          }
-        }
-      }
-    },
-
-    // // DEV
-    compass: {
+  
+    sass: {
       dev: {
         options: {
-          sassDir: 'dev/scss',
-          cssDir: 'atv/css'
-        }
+          outputStyle: 'compact'
+        },
+        files: [{
+          expand: true,
+          cwd: 'dev/css/',
+          src: ['*.scss'],
+          dest: 'css/',
+          ext: '.css'
+        }]
       }
     },
-
-    // Monitoramento de arquivos
+    
+    cssmin: {
+      minify: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]        
+      }
+    },
+    
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg,gif,JPG}'],
+          dest: 'img/'
+        }]
+      }
+    },
+    
+    autoprefixer: {
+      dev: {
+        options: {
+          browsers: ["last 4 versions", "ios 6","ie 9"]
+        },
+        expand: true,
+        cwd: 'css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'css/'
+      }
+    },
+      
     watch: {
-      dev: {
-        files: ['.rebooted','sys/**/*'],
+      sass: {
+        files: 'dev/sass/**/*.scss',
+        tasks: ['sass:dev','autoprefixer:dev','cssmin:minify']
+      },  
+      livereload: {
+        files: ['**/*.html','js/*.js','dev/**/*'],
         options: {
           livereload: true
         }
       },
-      css: {
-        files: '**/*.scss',
-        tasks: ['compass'],
-        options: {
-          livereload: true
-        }
-      }
     },
   });
-
-  grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  
+  // Default
+  grunt.registerTask('default', 'watch');
 };
